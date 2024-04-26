b86b82a484327171d35eced4478fc6fbfba08908
osteise <164857506+a23oskme@users.noreply.github.com>
2024-04-26 15:49:31 +0200

    Removed some test data

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -17,36 +17,19 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
-    private final String JSON_FILE = "mountains.json";
+    //private final String JSON_FILE = "mountains.json";
 
-
-
-    ArrayList<MyRecyclerView> minTestText = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mountainList.add(new Mountain("Hej", "Hejsan!", 2));
-        //mountainList.add(new Mountain("Nytt", "Hejdå!", 4));
-
-        setUpMyRecyclerView();
-
-
 
         //new JsonFile(this, this).execute(JSON_FILE);
         new JsonTask(this).execute(JSON_URL);
     }
 
-    private void setUpMyRecyclerView() {
-        String[] testTexter = getResources().getStringArray(R.array.test_texter);
-
-        for (int i = 0; i<testTexter.length; i++) {
-            minTestText.add(new MyRecyclerView(testTexter[i]));
-        }
-
-    }
 
     @Override
     public void onPostExecute(String json) {
